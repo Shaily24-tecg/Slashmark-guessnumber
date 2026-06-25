d1d41c8869f310705aeed5b2413541fe028aeb1f
Shaily <shaily15112005@gamil.com>
2026-06-25 09:43:36 +0530

    there are only 6 chances for better game

--- main/Guess_number_game.py
+++ main/Guess_number_game.py
@@ -18,7 +18,7 @@ def pick(name):  # FIX: name passed here
     guessesTaken = 0
     number = random.randint(1, 200)  # FIX: moved inside function
 
-    while guessesTaken < 10:
+    while guessesTaken < 6:
         time.sleep(.25)
         enter = input("Guess: ")
 
@@ -29,7 +29,7 @@ def pick(name):  # FIX: name passed here
                 guessesTaken = guessesTaken + 1
 
                 # CHANCES DISPLAY
-                print("Remaining chances:", 10 - guessesTaken)
+                print("Remaining chances:", 6 - guessesTaken)
 
                 if guess < number:
                     print("The guess is too low")
@@ -40,10 +40,9 @@ def pick(name):  # FIX: name passed here
                 else:
                     break
 
-                if guessesTaken < 10:
+                if guessesTaken < 6:
                     time.sleep(.5)
                     print("Try Again!")
-
             else:
                 print("Silly Goose! That number is out of range!")
                 time.sleep(.25)
